fa5f9ba1c8be7b9cbb5f590dea4930a3411aed65
daszybak <karlo1diidd@gmail.com>
2023-03-14 22:49:58 +0100

    Fix setting up services

--- src/reservations/reservations.module.ts
+++ src/reservations/reservations.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { ReservationService } from './reservations.service';
+import { ReservationsController } from './reservations.controller';
 
 @Module({
-	providers: [PrismaService],
-	controllers: [ReservationService],
+	providers: [PrismaService, ReservationService],
+	controllers: [ReservationsController],
 	exports: [ReservationService],
 })
 export class ReservationModule {}
--- src/reservations/reservations.service.ts
+++ src/reservations/reservations.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma.service';
 import { Reservation, Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class ReservationService {
